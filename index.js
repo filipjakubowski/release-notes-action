590145e5f733a352e13861826236796481ff42c6
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2022-11-29 16:08:41 +0100

    WP-40 test

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const notes =  require('@bamboostick/git_release_notes');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const notesString = notes.getReleaseNotesString("ab33640");
+    core.setOutput('notes', notesString);
   } catch (error) {
     core.setFailed(error.message);
   }
