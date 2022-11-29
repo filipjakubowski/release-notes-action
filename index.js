c5bcd1f85ad8e4536a63ee4521155ce582cc4a2c
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2022-11-29 16:16:04 +0100

    add output

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const notes =  require('@bamboostick/git_release_notes');
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const notesString = notes.getReleaseNotesString("ab33640");
+    const notesString = notes.getReleaseNotesString("ab33640")
+    console.log("Notes: ");
+    console.log(notes);
     core.setOutput('notes', notesString);
   } catch (error) {
     core.setFailed(error.message);
