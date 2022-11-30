5d97264a8914e8a7c2d080fa5c865bf783f80fe4
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2022-11-30 21:30:19 +0100

    refs

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function run() {
     console.log(github.context.payload.after);
     console.log(github.context.payload.pull_request.base.sha)
 
-    const args = ["log", "--format=oneline", `${github.context.ref}..${github.context.payload.pull_request.base.ref}`];
+    const args = ["log", "--format=oneline", `${github.context.payload.pull_request.head.ref}..${github.context.payload.pull_request.base.ref}`];
 
     try{
       await exec.exec("git", args);
