1d7814f9155e09f49a85266b7fd2207c1c8539eb
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2022-11-30 21:18:04 +0100

    debug

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const notes =  require('@bamboostick/git_release_notes');
 const github = require('@actions/github');
+import * as exec from "@actions/exec";
+
 
 // most @actions toolkit packages have async methods
 async function run() {
@@ -11,8 +13,22 @@ async function run() {
     console.log(github.context.payload.pull_request.base);
     console.log(github.context.payload.before);
     console.log("-----------------");
+
+
     console.log(github.context.payload.after);
     console.log(github.context.payload.pull_request.base.sha)
+
+    const args: string[] = ["log", "--format=oneline", `${github.context.payload.pull_request.base.sha}..`];
+
+    try{
+      await exec.exec("git", args);
+    }
+    catch (error){
+      console.log("Error while git log. ");
+      console.log(error);
+    }
+
+
     const fromRef = github.context.payload.pull_request.base.sha;
     const toRef = github.context.payload.after;
     const notesString = notes.releaseNotesString(fromRef, toRef);
