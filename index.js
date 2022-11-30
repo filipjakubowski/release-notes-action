f0d055bfb495afaa0bc192297d92b3b93378a911
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2022-11-30 19:25:17 +0100

    combined github context

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const core = require('@actions/core');
 const notes =  require('@bamboostick/git_release_notes');
+const github = require('@actions/github');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     console.log("Running ")
-    const notesString = notes.releaseNotesString("ab33640")
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const fromRef = github.context.base_ref;
+    const toRef = github.context.ref;
+    const notesString = notes.releaseNotesString(fromRef, toRef);
     console.log("Notes: @bamboostick/git_release_notes");
     console.log(notesString);
     core.setOutput('notes', notesString);
