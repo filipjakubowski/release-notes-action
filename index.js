0e22255fb68c005b2cd3429e35fb9b9c0eed5e84
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2022-12-11 19:59:51 +0100

    WP-40

--- index.js
+++ index.js
@@ -5,33 +5,8 @@ const github = require('@actions/github');
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-
     const eventName = github.context.eventName;
-
-    console.log(eventName);
     console.log(`Preparing Release Notes for action: ${eventName}`);
-    console.log(`context:`);
-    console.log(github.context);
-    console.log(`--------------------------------`);
-
-
-  //
-  //
-  //   console.log(github.context.payload.after);
-  //   console.log(github.context.payload.pull_request.base.sha)
-  //
-  //   const args = ["log", "--format=oneline", `${github.context.payload.pull_request.base.ref}..${github.context.payload.pull_request.head.ref}`];
-  //
-  //   try{
-  //     await exec.exec("git", args);
-  //   }
-  //   catch (error){
-  //     console.log("Error while git log. ");
-  //     console.log(error);
-  //   }
-
-    let fromRef = "";
-    let toRef = "";
 
     switch(eventName){
       case 'push':{
@@ -43,15 +18,14 @@ async function run() {
         break;
       }
       case 'pull_request':{
-        console.log("\----------BASE-------------")
-        console.log(github.context.payload.pull_request.base);
-        console.log("\----------HEAD-------------")
-        console.log(github.context.payload.pull_request.head);
-        console.log(github.context.payload.before);
-        console.log("-----------------");
-
-        fromRef = github.context.payload.pull_request.base.sha;
-        toRef = github.context.payload.after;
+        // console.log("\----------BASE-------------")
+        // console.log(github.context.payload.pull_request.base);
+        // console.log("\----------HEAD-------------")
+        // console.log(github.context.payload.pull_request.head);
+        // console.log(github.context.payload.before);
+        // console.log("-----------------");
+        let fromRef = github.context.payload.pull_request.base.sha;
+        let toRef = github.context.payload.after;
         const notesString = notes.releaseNotesString(fromRef, toRef);
         core.setOutput('notes', notesString);
         break;
